1115cc9c6e2be05d4718ac0d7a4755d30fc2a761
Youssef Mansour <youssefmansour@aucegypt.edu>
2024-11-29 15:53:08 +0100

    automate rewriting

--- scripts/rewrite_texts_batch_auto.py
+++ scripts/rewrite_texts_batch_auto.py
@@ -1,4 +1,6 @@
-#python rewrite_texts_batch_auto.py --input-file C4.jsonl --output-file C4_prompt3_rewritten.jsonl
+# An OpenAI account is required, and the API key should be set as an environment variable. 
+# export OPENAI_API_KEY=
+
 
 import time
 import json
@@ -6,17 +8,19 @@ import argparse
 import logging
 from openai import OpenAI
 
-prompt1= "Rewrite the following text sentence by sentence while preserving its length and the accuracy of its content. Maintain the overall format, structure, and flow of the text: \n "
-prompt2= "Rewrite the following text while preserving its length and the accuracy of its content: \n "
-prompt3= "Rewrite the following text while preserving its length and the accuracy of its content. Do not use newlines, new paragraphs, itemization, enumeration, and other formatting, unless it is important or appropriate for better readability: \n "
+# Predefined prompts
+PROMPTS = {
+    "prompt1": "Rewrite the following text sentence by sentence while preserving its length and the accuracy of its content. Maintain the overall format, structure, and flow of the text: \n ",
+    "prompt2": "Rewrite the following text while preserving its length and the accuracy of its content: \n ",
+    "prompt3": "Rewrite the following text while preserving its length and the accuracy of its content. Do not use newlines, new paragraphs, itemization, enumeration, and other formatting, unless it is important or appropriate for better readability: \n "
+}
 
-prompt = prompt3
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Function to submit a new batch
-def submit_batch(client, input_data):
+def submit_batch(client, input_data, prompt):
     # Generate the batch file name
     batch_file_name = "batch_input.jsonl"
 
@@ -80,9 +84,13 @@ def main():
     parser.add_argument('--input-file', type=str, required=True, help='Path to the jsonl input file, containing the texts to rewrite')
     parser.add_argument('--output-file', type=str, required=True, help='Path to the jsonl output file for writing the rewritten texts to')
     parser.add_argument('--batch-size', type=int, default=2000, help='Number of lines to process in each batch')
+    parser.add_argument('--prompt', type=str, choices=['prompt1', 'prompt2', 'prompt3'], required=True, help='Select the rewriting prompt: prompt1, prompt2, or prompt3')
 
     args = parser.parse_args()
     logging.info(f"Arguments received: {args}")
+
+    # Select the appropriate prompt
+    prompt = PROMPTS[args.prompt]
 
     client = OpenAI()
 
